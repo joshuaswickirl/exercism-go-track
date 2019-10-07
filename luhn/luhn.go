0bbb3f754bbd6f1c35396823f1d1661ae9ea8406
Joshua Swick <joshuaswickirl@gmail.com>
2019-10-07 08:37:32 -0600

    Refactor to remove else and numIndexes var

--- luhn/luhn.go
+++ luhn/luhn.go
@@ -7,39 +7,25 @@ import (
 // Valid checks if a credit card number is valid
 func Valid(cardNumberInput string) bool {
 	cardNumber := strings.ReplaceAll(cardNumberInput, " ", "")
-
 	if len(cardNumber) <= 1 {
 		return false
 	}
-
 	cardNumberSlice := []rune(cardNumber)
-
 	var sum int
 	var everyOther bool
-	numIndexes := len(cardNumberSlice) - 1
-
-	for i := numIndexes; i >= 0; i-- {
-
+	for i := len(cardNumberSlice) - 1; i >= 0; i-- {
 		value := int(cardNumberSlice[i] - '0')
-
 		if value < 0 || value > 9 {
 			return false
 		}
-
-		if !everyOther {
-			sum += value
-		} else {
-
-			newValue := value * 2
-			if newValue > 9 {
-				newValue = newValue - 9
+		if everyOther {
+			value = value * 2
+			if value > 9 {
+				value = value - 9
 			}
-
-			sum += newValue
 		}
-
+		sum += value
 		everyOther = !everyOther
 	}
-
 	return sum%10 == 0
 }
