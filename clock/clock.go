2fc12c192b7c1f72bc5ff766b7ae950830e8aeee
Joshua Swick <joshuaswickirl@gmail.com>
2019-10-13 07:53:38 -0600

    Enhancement to use minutes and move Adjust() logic to New()

--- clock/clock.go
+++ clock/clock.go
@@ -2,65 +2,32 @@ package clock
 
 import (
 	"fmt"
-	"math"
 )
 
-// Clock has hours and minutes
-type Clock struct {
-	hours   int
-	minutes int
-}
+// Clock is the time in minutes
+type Clock int
 
 // New creates a new clock
 func New(hours, minutes int) Clock {
-	c := Clock{hours, minutes}
-	c.Adjust()
-	return c
+	minutesInDay := 1440
+	c := (hours*60 + minutes) % minutesInDay
+	if c < 0 {
+		c += minutesInDay
+	}
+	return Clock(c)
 }
 
 // Add seconds to the clock
 func (c Clock) Add(minutes int) Clock {
-	c.minutes += minutes
-	c.Adjust()
-	return c
+	return New(0, int(c)+minutes)
 }
 
 // Subtract seconds from the clock
 func (c Clock) Subtract(minutes int) Clock {
-	c.minutes -= minutes
-	c.Adjust()
-	return c
+	return New(0, int(c)-minutes)
 }
 
 // String returns the formatted time
 func (c Clock) String() string {
-	return fmt.Sprintf("%02d:%02d", c.hours, c.minutes)
-}
-
-// Adjust corrects the time to a 24 hour clock
-func (c *Clock) Adjust() {
-	for {
-		var changes bool
-		if c.minutes >= 60 {
-			c.hours += c.minutes / 60
-			c.minutes = c.minutes % 60
-			changes = true
-		}
-		if c.minutes < 0 {
-			c.hours -= int(math.Abs(float64(c.minutes/60))) + 1
-			c.minutes = 60 - int(math.Abs(float64(c.minutes)))%60
-			changes = true
-		}
-		if c.hours >= 24 {
-			c.hours = c.hours % 24
-			changes = true
-		}
-		if c.hours < 0 {
-			c.hours = 24 - int(math.Abs(float64(c.hours%24)))
-			changes = true
-		}
-		if !changes {
-			break
-		}
-	}
+	return fmt.Sprintf("%02d:%02d", c/60, c%60)
 }
