1a90de2adfa78648614be7806b593b3d5c42d88b
Joshua Swick <joshuaswickirl@gmail.com>
2020-03-14 16:30:39 -0600

    Refactor for readability

--- triangle/triangle.go
+++ triangle/triangle.go
@@ -4,34 +4,35 @@ package triangle
 import "math"
 
 // Kind is the type of triangle
-type Kind string
+type Kind int
 
+// Types of triangles
 const (
-	// NaT is not a triangle
-	NaT = "NaT"
-	// Equ is an quilateral triangle
-	Equ = "Equ"
-	// Iso is an isosceles triangle
-	Iso = "Iso"
-	// Sca is a scalene triangle
-	Sca = "Sca"
+	NaT Kind = iota // Not a triangle
+	Equ             // Equilateral
+	Iso             // Isosceles
+	Sca             // Scalene
 )
 
-// KindFromSides ...
+// KindFromSides returns the type of triangle, given three sides
 func KindFromSides(a, b, c float64) Kind {
-	if a == math.Inf(1) || b == math.Inf(1) || a == math.NaN() ||
-		b == math.NaN() || a == math.Inf(-1) || b == math.Inf(-1) {
+	if a+b < c || b+c < a || c+a < b ||
+		IsInvalidSide(a) || IsInvalidSide(b) || IsInvalidSide(c) {
 		return NaT
 	}
-	if a+b >= c && b+c >= a && c+a >= b && a+b+c != 0 {
-		if a == b && a == c {
-			return Equ
-		}
-		if a == b || a == c || b == c {
-			return Iso
-		}
-		return Sca
+	if a == b && a == c {
+		return Equ
 	}
+	if a == b || a == c || b == c {
+		return Iso
+	}
+	return Sca
+}
 
-	return NaT
+// IsInvalidSide returns true if the side is 0, NaN, or Inf
+func IsInvalidSide(side float64) bool {
+	if side == 0 || math.IsInf(side, 0) || math.IsNaN(side) {
+		return true
+	}
+	return false
 }
