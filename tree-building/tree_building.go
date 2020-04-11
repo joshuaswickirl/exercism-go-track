995a83440c5fdfb68dd2ec2dc5cb0013ed1852ce
Joshua Swick <joshuaswickirl@gmail.com>
2020-04-11 09:59:48 -0600

    Refactor solution to sort records first.
    This allows the records to be validated more simply
    during the loop.

--- tree-building/tree_building.go
+++ tree-building/tree_building.go
@@ -18,46 +18,23 @@ type Node struct {
 }
 
 // Build returns the root of a tree recreated from a slice of records.
+// An error is returned if the IDs are not contiguous, increasing from 0,
+// or a non-root ID has a parent ID equal to or smaller than itself.
 func Build(records []Record) (*Node, error) {
-	if len(records) == 0 {
-		return nil, nil
-	}
-
-	// Prepare for building the tree by creating a map of IDs and their
-	// *Node. Detect non-continous ID's by comparing the GaussianSum to
-	// the actual sum of the ID's.
-	nodeByID := map[int]*Node{}
-	GaussianSum := (float64(len(records)) / 2) * (0 + float64(len(records)) - 1)
-	sumOfIDs := 0.0
-	for _, record := range records {
-		if _, ok := nodeByID[record.ID]; ok {
-			return &Node{}, errors.New("duplicate node")
-		} else if record.ID == 0 && record.Parent != 0 {
-			return &Node{}, errors.New("root node has parent")
-		} else if record.ID <= record.Parent && record.ID != 0 {
-			return &Node{}, errors.New("record has impossible child")
+	nodes := make(map[int]*Node, len(records))
+	sort.SliceStable(records, func(i, j int) bool {
+		return records[i].ID < records[j].ID
+	})
+	for i, r := range records {
+		if r.ID != i || r.ID < r.Parent || (r.ID != 0 && r.ID == r.Parent) {
+			return &Node{}, errors.New("invalid records")
 		}
-		nodeByID[record.ID] = &Node{ID: record.ID}
-		sumOfIDs += float64(record.ID)
-	}
-	if sumOfIDs != GaussianSum {
-		return &Node{}, errors.New("non-continuous ID's")
-	}
-
-	// Build a tree and return the root.
-	root, ok := nodeByID[0]
-	if !ok {
-		return &Node{}, errors.New("no root node")
-	}
-	for _, record := range records {
-		if record.ID == 0 {
+		nodes[r.ID] = &Node{ID: r.ID}
+		if r.ID == 0 {
 			continue
 		}
-		parent := nodeByID[record.Parent]
-		parent.Children = append(parent.Children, nodeByID[record.ID])
-		sort.SliceStable(parent.Children, func(i, j int) bool {
-			return parent.Children[i].ID < parent.Children[j].ID
-		})
+		parent := nodes[r.Parent]
+		parent.Children = append(parent.Children, nodes[r.ID])
 	}
-	return root, nil
+	return nodes[0], nil
 }
