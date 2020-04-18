c9df00892c4f93bea11131cb9e8052810652ee57
Joshua Swick <joshuaswickirl@gmail.com>
2020-04-18 09:27:14 -0600

    Use struct fields instead of methods and plain strings.Split.

--- tournament/tournament.go
+++ tournament/tournament.go
@@ -10,17 +10,11 @@ import (
 )
 
 type record struct {
-	wins   int
-	draws  int
-	losses int
-}
-
-func (r record) matches() int {
-	return r.wins + r.draws + r.losses
-}
-
-func (r record) points() int {
-	return 3*r.wins + r.draws
+	wins    int
+	draws   int
+	losses  int
+	matches int
+	points  int
 }
 
 // Tally writes the given match results as a formatted table, returning
@@ -36,7 +30,7 @@ func Tally(r io.Reader, w io.Writer) error {
 		if line == "" || line[0] == '#' {
 			continue
 		}
-		tokens := strings.SplitN(line, ";", -1)
+		tokens := strings.Split(line, ";")
 		if len(tokens) != 3 {
 			return errors.New("bad results format")
 		}
@@ -48,16 +42,22 @@ func Tally(r io.Reader, w io.Writer) error {
 		if !team2Ok {
 			teams = append(teams, tokens[1])
 		}
+		team1Record.matches++
+		team2Record.matches++
 		switch tokens[2] {
 		case "win":
 			team1Record.wins++
+			team1Record.points += 3
 			team2Record.losses++
 		case "loss":
 			team1Record.losses++
 			team2Record.wins++
+			team2Record.points += 3
 		case "draw":
 			team1Record.draws++
+			team1Record.points++
 			team2Record.draws++
+			team2Record.points++
 		default:
 			return errors.New("bad match result")
 		}
@@ -66,7 +66,7 @@ func Tally(r io.Reader, w io.Writer) error {
 	}
 	// sort by wins, break ties alphabetically
 	sort.SliceStable(teams, func(i, j int) bool {
-		if tally[teams[i]].points() == tally[teams[j]].points() {
+		if tally[teams[i]].points == tally[teams[j]].points {
 			name1 := strings.ToLower(teams[i])
 			name2 := strings.ToLower(teams[j])
 			for k := 0; k < len(name1); k++ {
@@ -79,15 +79,15 @@ func Tally(r io.Reader, w io.Writer) error {
 			}
 			return true
 		}
-		return tally[teams[i]].points() > tally[teams[j]].points()
+		return tally[teams[i]].points > tally[teams[j]].points
 	})
 	// write formatted table
 	fmt.Fprintf(w, "Team%s| MP |  W |  D |  L |  P\n", strings.Repeat(" ", 27))
 	for _, t := range teams {
 		r := tally[t]
 		fmt.Fprintf(w, "%v%s|  %v |  %v |  %v |  %v |  %v\n",
-			t, strings.Repeat(" ", 31-len(t)), r.matches(), r.wins, r.draws,
-			r.losses, r.points())
+			t, strings.Repeat(" ", 31-len(t)), r.matches, r.wins, r.draws,
+			r.losses, r.points)
 	}
 	return nil
 }
