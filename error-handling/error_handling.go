d7fc74b51ec9072512cbf702cffd2dabb6f14835
Joshua Swick <joshuaswickirl@gmail.com>
2020-04-23 07:33:04 -0600

    Simplify loop and add default to recovery switch

--- error-handling/error_handling.go
+++ error-handling/error_handling.go
@@ -1,29 +1,34 @@
 package erratum
 
-// Use opens a resource and recovers if it panics.
+import "fmt"
+
+// Use invokes a ResourceOpener and calls the resource's Frob method
+// passing the input.
+// Opening the resource is retried if there is a TransientError and
+// returns any other error. Panics are recovered and if the Panic is a
+// FrobError, the resource is Defrob'ed with the defrobTag from the error.
 func Use(o ResourceOpener, input string) (err error) {
-	var resource Resource
-	for {
-		resource, err = o()
-		if _, ok := err.(TransientError); ok {
-			continue
-		} else if err != nil {
-			return
+	resource, err := o()
+	for err != nil {
+		if _, ok := err.(TransientError); !ok {
+			return err
 		}
-		break
+		resource, err = o()
 	}
 	defer resource.Close()
 	defer func() {
 		if r := recover(); r != nil {
-			if e, ok := r.(FrobError); ok {
+			switch e := r.(type) {
+			case FrobError:
 				resource.Defrob(e.defrobTag)
 				err = e
-			}
-			if e, ok := r.(error); ok {
+			case error:
 				err = e
+			default:
+				err = fmt.Errorf("recovered value %v of type %T", e, e)
 			}
 		}
 	}()
 	resource.Frob(input)
-	return
+	return err
 }
