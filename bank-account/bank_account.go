9b68c65922e6062406021e2150aee9bf46f1a57b
Joshua Swick <joshuaswickirl@gmail.com>
2020-04-24 07:25:26 -0600

    Embed mutex in Account and update isClosed to closed

--- bank-account/bank_account.go
+++ bank-account/bank_account.go
@@ -4,9 +4,9 @@ import "sync"
 
 // Account is a bank account with a balance, open/closed state, and a mutex.
 type Account struct {
-	mux      sync.RWMutex
-	isClosed bool
-	balance  int64
+	sync.RWMutex
+	closed  bool
+	balance int64
 }
 
 // Open constructs a new Account. It returns nil if the inital deposit is
@@ -15,29 +15,29 @@ func Open(initialDeposit int64) *Account {
 	if initialDeposit < 0 {
 		return nil
 	}
-	return &Account{isClosed: false, balance: initialDeposit}
+	return &Account{closed: false, balance: initialDeposit}
 }
 
 // Close return pays out the balance and closes the account. If the account
 // is already closed it returns 0, false.
 func (a *Account) Close() (payout int64, ok bool) {
-	a.mux.Lock()
-	defer a.mux.Unlock()
-	if a.isClosed {
+	a.Lock()
+	defer a.Unlock()
+	if a.closed {
 		return 0, false
 	}
 	payout = a.balance
 	a.balance = 0
-	a.isClosed = true
+	a.closed = true
 	return payout, true
 }
 
 // Balance returns the current balance. If the Account is closed 0, false
 // is returned.
 func (a *Account) Balance() (balance int64, ok bool) {
-	a.mux.RLock()
-	defer a.mux.RUnlock()
-	if a.isClosed {
+	a.RLock()
+	defer a.RUnlock()
+	if a.closed {
 		return 0, false
 	}
 	return a.balance, true
@@ -47,9 +47,9 @@ func (a *Account) Balance() (balance int64, ok bool) {
 // would bring the balance below zero, the transaction is aborted. In this
 // case the exiting balance is returned with ok=false.
 func (a *Account) Deposit(amount int64) (newBalance int64, ok bool) {
-	a.mux.Lock()
-	defer a.mux.Unlock()
-	if a.isClosed {
+	a.Lock()
+	defer a.Unlock()
+	if a.closed {
 		return 0, false
 	}
 	if b := a.balance + amount; b < 0 {
